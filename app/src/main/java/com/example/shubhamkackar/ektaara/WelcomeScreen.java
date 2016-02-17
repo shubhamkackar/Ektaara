74bb01218c0d7f4033c9efa0ebe896cf306a722f
shubhamkackar <shubham.kackar@gmail.com>
2016-02-17 19:13:17 +0530

    Initial commit

---
+++ app/src/main/java/com/example/shubhamkackar/ektaara/WelcomeScreen.java
@@ -0,0 +1,76 @@
+package com.example.shubhamkackar.ektaara;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.TextView;
+// Add this to the header of your file:
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class WelcomeScreen extends Activity {
+
+
+
+    CallbackManager callbackManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        setContentView(R.layout.activity_welcome_screen);
+
+        //final TextView result=(TextView)findViewById(R.id.key);
+
+
+        callbackManager = CallbackManager.Factory.create();
+        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                //result.setText("Exit");
+            }
+
+            @Override
+            public void onCancel() {
+                //result.setText("Cancelled");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                //result.setText("Error");
+            }
+        });
+
+
+
+    }
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+}
